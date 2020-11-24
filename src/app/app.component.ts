925baf087301c0bd63a9c436aafe7e2fb60be807
Luis Muñoz <luenmuvel@gmail.com>
2020-11-24 15:14:36 -0500

    chore: añadiendo cambios sobre app.component para usar el router-outlet y marcar la posición para la directiva del loading.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ComponentFactoryResolver,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingComponent } from './components/loading/loading.component';
+import { LoadingDirective } from './directives/login.directive';
 
 @Component({
-  selector: 'app-root',
+  selector: 'bcp-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'BCPChallenge';
+  @ViewChild(LoadingDirective) public bcpLoading: LoadingDirective;
+
+  constructor(
+    private componenteFactoryResolver: ComponentFactoryResolver,
+    private router: Router
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.showLoading();
+  }
+
+  showLoading(): void {
+    const componentFactory = this.componenteFactoryResolver.resolveComponentFactory(
+      LoadingComponent
+    );
+    const viewContainerRef = this.bcpLoading.viewContainerRef;
+    this.removeLoading(viewContainerRef);
+    viewContainerRef.createComponent(componentFactory);
+
+    setTimeout(() => {
+      this.removeLoading(viewContainerRef);
+      if (this.router.url === '/') {
+        this.router.navigate(['/list']);
+      }
+    }, 2000);
+  }
+
+  removeLoading(viewContainerRef: ViewContainerRef): void {
+    viewContainerRef.clear();
+  }
 }
